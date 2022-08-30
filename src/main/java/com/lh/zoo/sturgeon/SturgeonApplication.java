8958ff438367dc9acfd19fea88890879502052cb
LiHe1634 <1399827270@qq.com>
2022-08-31 00:50:01 +0800

    lh feat 初始化项目

---
+++ src/main/java/com/lh/zoo/sturgeon/SturgeonApplication.java
@@ -0,0 +1,13 @@
+package com.lh.zoo.sturgeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SturgeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SturgeonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lh/zoo/sturgeon/SturgeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.zoo.sturgeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SturgeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
